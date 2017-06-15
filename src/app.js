c1aad80700a3886bedcffa5ca38b5f1e51425937
Duc Filan <ducfilan@gmail.com>
2017-06-15 22:32:49 +0900

    React to auth state changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Card, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { isLoggedIn: undefined, user: undefined }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAYJP96TMNtf5YuEgIlg94vnNhumi7VB9Y',
@@ -14,16 +16,62 @@ class App extends Component {
       storageBucket: 'authdemo-b0a9b.appspot.com',
       messagingSenderId: '378830739987'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+         this.setState({ isLoggedIn: true, user });
+      } else {
+        this.setState({ isLoggedIn: true });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.isLoggedIn) {
+      case true:
+        return (
+          <Card>
+            <Text style={styles.welcomeMessageStyle}>
+              Welcome {this.state.user.email}
+            </Text>
+            <CardSection>
+              <Button>Log out</Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={styles.spinnerWrapperStyle}>
+            <Spinner />
+          </View>
+        );
+    }
   }
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Header>Authentication</Header>
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  welcomeMessageStyle: {
+    alignSelf: 'center',
+    fontSize: 18,
+    marginTop: 20,
+    marginBottom: 20
+  },
+  spinnerWrapperStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+};
 
 export default App;
